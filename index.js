521db8cc2500eb4531c5641933a0af7af03aaba9
md-masum <masum.nextu@gmail.com>
2023-05-03 03:06:27 +0600

    cards json added

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+const chefscard = require('./data/chefscard.json');
 
 
 app.get('/', (req, res) => {
     res.send('chef is running')
 });
+
+app.use(cors());
+
+app.get('/chefscard', (req, res) => {
+    res.send(chefscard)
+})
 
 app.listen(port, () => {
     console.log(`Chef API is running on port ${port}`)
