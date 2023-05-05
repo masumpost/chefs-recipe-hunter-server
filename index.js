f3d01aa4ffd27fcc5ef0e3f6e464d60645c3b90d
md-masum <masum.nextu@gmail.com>
2023-05-05 20:08:43 +0600

    main json file added

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
-const chefscard = require('./data/chefscard.json');
+// const chefscard = require('./data/chefscard.json');
 const viewresi = require('./data/viewrecipes.json');
+const chefscard = require('./data/chef.json');
 
 app.get('/', (req, res) => {
     res.send('chef is running')
@@ -30,6 +31,11 @@ app.get('/viewresi/:id', (req, res) => {
     const id = req.params.id;
     const selectedresi = viewresi.find(n => n.id == id);
     res.send(selectedresi)
+})
+
+
+app.get('/chef', (req, res) => {
+    res.send(chef)
 })
 
 app.listen(port, () => {
